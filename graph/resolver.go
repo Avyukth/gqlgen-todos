92ce1e1971a106f552a78cfa048a2cfc17c9ec84
subha <subhrajit.makur@razorpay.com>
2020-04-16 18:40:39 +0530

    resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,10 +1,11 @@
 package graph
 
+import "github.com/avyukth/gqlgen-todos/graph/model"
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct{
+	todos []*model.Todo
 }
-
